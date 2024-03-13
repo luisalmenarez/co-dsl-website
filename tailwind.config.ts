ae27bfc590babc7e14b57dffd33418018c07f087
Luis Almenarez <luisalmenarez0428@gmail.com>
2024-03-12 22:49:41 -0500

    ✨ Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#62cbc9",
+        "gray-light": "#232028",
+        "yellow-hover": "#FFB703",
+        "color-contrast": "#A0AAB2",
       },
     },
   },
